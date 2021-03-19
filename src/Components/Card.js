c7a71a7971860a47fcbb811b0b9c1d39b4b1db06
ultramagnus <alex.dragonfox@gmail.com>
2021-03-19 20:07:41 +0200

    add image container, use svg in html instead <img>

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,14 +1,12 @@
 import "./Card.css";
 
+import ImageContainer from "./ImageContainer";
 import FAQList from "./FAQList";
 
 export default function Card() {
 	return (
 		<div id="card">
-			<div id="image-container">
-				<img id="women-image" src="/images/illustration-woman-online-desktop.svg" alt="women" />
-				<img id="box-image" src="/images/illustration-box-desktop.svg" alt="women" />
-			</div>
+			<ImageContainer />
 			<FAQList />
 		</div>
 	);
